7a457a39a3176a7264e4f3fc8017018058c4b9eb
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2020-12-25 14:29:18 +0100

    started sidebar

---
+++ src/components/SideBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sidebar-container d-flex flex-column align-items-center">
+    <div>
+      <img src="@/assets/img/Mask Group (4).png" alt="" class="image mb-2" />
+    </div>
+    <div class="d-flex mb-5">
+      <span>Henry Jabbawockiez</span><ArrowIcon />
+    </div>
+    <SideBarItems />
+  </div>
+</template>
+
+<script>
+import ArrowIcon from "./ArrowIcon.vue";
+import SideBarItems from "./SideBarItems.vue";
+export default {
+  components: {
+    ArrowIcon,
+    SideBarItems,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar-container {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  height: 100vh;
+  width: 15%;
+  background: #FFFFFF;
+
+  span {
+    font-size: 18px;
+  }
+}
+.image {
+  padding-top: 92px;
+}
+</style>
---
+++ src/components/SideBarItems.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <SideBarItem />
+    <HomeIcon />
+  </div>
+</template>
+
+<script>
+import HomeIcon from "./HomeIcon.vue";
+import SideBarItem from "./SideBarItem.vue";
+
+export default {
+  components: {
+    SideBarItem,
+    HomeIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
