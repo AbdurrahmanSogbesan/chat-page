ca2a1915602d62459e04f097395991b785c4e1dc
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2020-12-27 02:11:35 +0100

    added dropdown component and story

---
+++ src/components/DropDown/DropDown.stories.js
@@ -0,0 +1,18 @@
+import DropDown from "./DropDown.vue";
+
+export default {
+  title: "DropDown/DropDown",
+  component: DropDown,
+  argTypes: {
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { DropDown },
+  template: '<DropDown v-bind="$props" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+};
---
+++ src/components/DropDown/DropDown.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <select name="Select" class="dropdown">
+      <option value="Messages">Messages</option>
+    </select>
+    <ArrowIcon class="icon" />
+  </div>
+</template>
+
+<script>
+import ArrowIcon from "../ArrowIcon/ArrowIcon.vue";
+export default {
+  components: {
+    ArrowIcon,
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.dropdown {
+  width: 179px;
+  height: 70px;
+  font-size: 18px;
+  padding: 22px 36px;
+  outline: none;
+  border-radius: 6px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border: none;
+}
+
+.icon {
+  position: relative;
+  top: -47px;
+  left: 124px;
+}
+</style>
