1a0d97275a9cc29fe15b52364d54e948885220b2
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2020-12-30 01:20:37 +0100

    added attachment card component and story

---
+++ src/components/AttachmentCard/AttachmentCard.stories.js
@@ -0,0 +1,24 @@
+import AttachmentCard from "./AttachmentCard.vue";
+import AttachmentFileIcon from "../AttachmentFileIcon.vue";
+
+export default {
+  title: "AttachmentCard/AttachmentCard",
+  component: AttachmentCard,
+  argTypes: {
+    background: { control: "color" },
+    color: { control: "color" },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { AttachmentCard },
+  template: '<AttachmentCard v-bind="$props" />',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  cards: [{ icon: AttachmentFileIcon, text: "Files (X2)" }],
+  background: "rgba(42, 139, 242, 0.25)",
+  color: "#2a8bf2",
+};
